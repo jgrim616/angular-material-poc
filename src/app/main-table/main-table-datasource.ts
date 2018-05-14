c03a69b8968fb696001ef1ef2052fe4ea7a0659e
Jim Grimaldi <jgrimaldi@healthesystems.com>
2018-05-14 15:26:43 -0400

    - Committing changes before starting on Buttons screen

--- src/app/main-table/main-table-datasource.ts
+++ src/app/main-table/main-table-datasource.ts
@@ -11,26 +11,32 @@ export interface MainTableItem {
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: MainTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  { id: 1, name: 'Hydrogen' },
+  { id: 2, name: 'Helium' },
+  { id: 3, name: 'Lithium' },
+  { id: 4, name: 'Beryllium' },
+  { id: 5, name: 'Boron' },
+  { id: 6, name: 'Carbon' },
+  { id: 7, name: 'Nitrogen' },
+  { id: 8, name: 'Oxygen' },
+  { id: 9, name: 'Fluorine' },
+  { id: 10, name: 'Neon' },
+  { id: 11, name: 'Sodium' },
+  { id: 12, name: 'Magnesium' },
+  { id: 13, name: 'Aluminum' },
+  { id: 14, name: 'Silicon' },
+  { id: 15, name: 'Phosphorus' },
+  { id: 16, name: 'Sulfur' },
+  { id: 17, name: 'Chlorine' },
+  { id: 18, name: 'Argon' },
+  { id: 19, name: 'Potassium' },
+  { id: 20, name: 'Calcium' },
+  { id: 21, name: 'Test1' },
+  { id: 22, name: 'Test2' },
+  { id: 23, name: 'Test3' },
+  { id: 24, name: 'Test4' },
+  { id: 25, name: 'Test5' },
+  { id: 26, name: 'Test6' }
 ];
 
 /**
@@ -62,9 +68,11 @@ export class MainTableDataSource extends DataSource<MainTableItem> {
     // Set the paginators length
     this.paginator.length = this.data.length;
 
-    return merge(...dataMutations).pipe(map(() => {
-      return this.getPagedData(this.getSortedData([...this.data]));
-    }));
+    return merge(...dataMutations).pipe(
+      map(() => {
+        return this.getPagedData(this.getSortedData([...this.data]));
+      })
+    );
   }
 
   /**
@@ -94,9 +102,12 @@ export class MainTableDataSource extends DataSource<MainTableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'id':
+          return compare(+a.id, +b.id, isAsc);
+        default:
+          return 0;
       }
     });
   }
